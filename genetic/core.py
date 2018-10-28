8d7989b0f8bc186b42174c38cae60131f7c562e3
bogdandm <bogdan.dm1995@yandex.ru>
2018-10-28 16:09:03 +0300

    Fix

--- genetic/core.py
+++ genetic/core.py
@@ -212,6 +212,8 @@ class GeneticOptimizer:
                     self.db.save_new_generation(self.core.evolve_generation(bots, self.g_number),
                                                 self.core.bot_class.version)
                     self.g_number = self.db.last_generation
+                else:
+                    self.g_number += 1
 
     def print(self):
         bots = [(bot.args_dict, bot.halite) for bot in self.db.load_generation(self.db.last_generation)]
--- run_vs_backup.bat
+++ run_vs_backup.bat
@@ -1,4 +1,4 @@
 halite.exe --replay-directory replays/ -vvv --width 46 --height 46 --"no-timeout"^
     "python MyBot.py"^
-    "cd backups\v7 & python MyBot.py"
+    "cd benchmark & python BenchmarkCollector.py"
 :: --v2
